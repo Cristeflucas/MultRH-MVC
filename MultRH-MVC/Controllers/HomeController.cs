bd0372dcfca07242c0ec8c46a05dd5590e7d579c
Cristeflucas <cristeflucas@hotmail.com>
2024-08-21 16:31:39 -0300

    Criando button e comecando section de services

--- MultRH-MVC/Controllers/HomeController.cs
+++ MultRH-MVC/Controllers/HomeController.cs
@@ -19,12 +19,13 @@ namespace MultRH_MVC.Controllers
             var images = new List<ImageModel>
             {
                 new ImageModel { ImageUrl = "/images/carousel1.png", Caption = "NÃO PERCA SEU TEMPO", Description = "Faça o cadastro de seu currículo hoje mesmo em nosso banco de dados e vá direto para entrevista na empresa contratante."},
-                new ImageModel { ImageUrl = "/images/carousel2.png", Caption = "CURSO DE INFORMÁTICA"},
-                new ImageModel { ImageUrl = "/images/carousel3.png", Caption = "CURSO DE AGENTE DE PORTARIA"}
+                new ImageModel { ImageUrl = "/images/carousel2.png", Caption = "CURSO DE INFORMÁTICA", Description = "O curso de informática presencial de 80h da MRH CURSOS tem como objetivo, preparar o profissional para o mercado de trabalho, usando de forma correta e eficiente o Windows, word, excel, internet, power point e digitação."},
+                new ImageModel { ImageUrl = "/images/carousel3.png", Caption = "CURSO DE AGENTE DE PORTARIA", Description = "Curso online de 40h para Porteiro, Agente de portaria, Recepcionista, Fiscal, Operador de estacionamento."},
+               
             };
             return View(images);
         }
-
+     
         public IActionResult Privacy()
         {
             return View();
---
+++ MultRH-MVC/Controllers/ServicesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultRH_MVC.Models;
+
+namespace MultRH_MVC.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var services = new List<ImageModel>
+            {
+                new ImageModel {ImageUrl = "/images/bck-curriculos.png"},
+                new ImageModel {ImageUrl = "/images/bck-services1.png"},
+                new ImageModel {ImageUrl = "/images/bck-services2.png"}
+            };
+            return View();
+        }
+    }
+}
